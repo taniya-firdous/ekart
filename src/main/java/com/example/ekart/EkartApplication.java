7587647f515ebdf446f94efc91d19549f3ef79a9
taniya-firdous <taniya.firdous0703@gmail.com>
2026-04-16 11:53:35 +0530

    added application.properties ,initialize security config

--- src/main/java/com/example/ekart/EkartApplication.java
+++ src/main/java/com/example/ekart/EkartApplication.java
@@ -1,9 +1,11 @@
 package com.example.ekart;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.ekart.mapper")
 public class EkartApplication {
 
 	public static void main(String[] args) {
