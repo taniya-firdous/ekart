c9f9ea8df28fcf22657e7ba012f087f73cae0ab4
taniya-firdous <taniya.firdous0703@gmail.com>
2026-04-20 17:47:43 +0530

    improved Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
+import "./App.css";
 function App() {
   return (
     <Routes>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,66 @@
+import Navbar from "../components/Navbar";
+import ProductCard from "../components/ProductCard"; // ✅ important
+import "./Home.css";
+
 function Home() {
+
+  const products = [
+    {
+      id: 1,
+      name: "iPhone 15",
+      price: 79999,
+      image: "https://via.placeholder.com/200"
+    },
+    {
+      id: 2,
+      name: "Laptop",
+      price: 55999,
+      image: "https://via.placeholder.com/200"
+    },
+    {
+      id: 3,
+      name: "Headphones",
+      price: 2999,
+      image: "https://via.placeholder.com/200"
+    },
+    {
+      id: 4,
+      name: "Shoes",
+      price: 1999,
+      image: "https://via.placeholder.com/200"
+    }
+  ];
+
   return (
-    <div className="page-container">
-      <div className="card">
-        <h2>Welcome to eKart 🎉</h2>
+    <div>
+      <Navbar />
+
+      {/* MENU BAR */}
+      <div className="menu-bar">
+        <span>All</span>
+        <span>Electronics</span>
+        <span>Fashion</span>
+        <span>Home</span>
+        <span>Appliances</span>
       </div>
+
+      {/* BANNER */}
+      <div className="banner">
+        <h2>Welcome to eKart</h2>
+        <p>Best deals available here</p>
+      </div>
+
+      {/* PRODUCT GRID */}
+      <div className="home">
+        <h2 className="home-title">Top Deals</h2>
+
+        <div className="product-grid">
+          {products.map((p) => (
+            <ProductCard key={p.id} product={p} />
+          ))}
+        </div>
+      </div>
+
     </div>
   );
 }
